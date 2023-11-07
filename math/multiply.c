41a473017e8c13b85c66278c2bb1196992a6c993
Federico-Lauricella <lauricellafederico@gmail.com>
2023-11-07 19:25:25 +0100

    funzione multiply

--- math/multiply.c
+++ math/multiply.c
@@ -1,5 +1,14 @@
 #include "math.h"
 
-int multiply(int a, int b) {
-    // stub!
+
+int multiply(int a, int b)
+
+{
+int risultato;
+
+for (int i=0; i<b; i++)
+{
+    risultato+=a;
+}
+return risultato;
 }
